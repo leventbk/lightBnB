b2667f4bc6223588292b1abd0d513c652a64f3f0
Leventbk <leventbkarakus@hotmail.com>
2023-08-11 14:42:28 -0400

    Get details about a single user

---
+++ 1_queries/01_singleuser.sql
@@ -0,0 +1,5 @@
+-- Select their id, name, email, and password.
+-- Select a single user using their email address. Use tristanjacobs@gmail.com for now.
+SELECT id, name, email, password
+FROM users
+WHERE email = 'tristanjacobs@gmail.com';
